a0d3001b85b213414c584c90e6d4f3ce2c735e57
Lucas-Berglund <42962292+Lucas-Berglund@users.noreply.github.com>
2019-02-04 05:28:20 +0100

    Reuploaded files.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './public'),
-    publicPath: "./public",
+    publicPath: "./",
     filename: "bundle.js",
     chunkFilename: '[name].js'
   },
